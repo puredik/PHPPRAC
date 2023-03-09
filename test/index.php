ae88580e642bb887a3b315c54420707a54e5914e
dhdsh <DH@github.com>
2023-03-09 08:52:13 +0530

    commit1

---
+++ test/index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>TSS-Technical Support System</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<script>
+   
+
+</script>
+
+<body>
+    <nav class="navbar navbar-inverse navbar-expand-sm bg-dark navbar-dark" style="display:flex;">
+        <div class="container-fluid">
+            <div class="navbar-nav navbar-left ">
+                <a class="navbar-brand" href="#">TSS</a>
+            </div>
+            <div class="nav navbar-nav navbar-left" id="collapsibleNavbar">
+                <ul class="navbar-nav navbar-left">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMPUTER</a>
+                    </li>zz
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CAR</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">HUMOR</a>
+                    </li>
+
+
+                    <input type="text" placeholder="More Topics">
+                    <button>Search</button>
+                </ul>
+
+            </div>
+            <div class="nav navbar-nav navbar-right" id="collapsibleNavbar">
+
+
+                <ul class="nav navbar-nav ">
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li>&nbsp;&nbsp;&nbsp;</li>
+                    <li><a href="#"><span class="glyphicon glyphicon-user  navbar-right"></span> Sign Up</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-2">
+                <h2>ABOUT TSS</h2>
+                <h5>Post Your Own Problem!</h5>
+                <p>Technical Solution System will provide expert solution for your problem</p>
+
+
+                <h2>NEWS</h2>
+                <p>C++ Basic Course Updated, Dec 7, 2022</pf>
+                <p>Python compact Course Launched, Dec 7, 2022</pf>
+                <p>Offline/Online Contacts</pf>
+
+
+
+                <h3 class="mt-4">TOPICS</h3>
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">PYTHON</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">JAVA</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">WEB</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">C++</a>
+                    </li>
+                </ul>
+
+
+
+            </div>
+
+            <div class="   col-md-10">
+                <div class="card text-center">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">User</th>
+                                <th scope="col">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Python crash printing math</td>
+                                <td>Otto</td>
+                                <td>01:00</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>C++ Compiler Setting up</td>
+                                <td>Thornton</td>
+                                <td>02:00</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Car Engine makes Weird sound</td>
+                                <td>Thornton</td>
+                                <td>03:00</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Car Engine makes Weird sound</td>
+                                <td>Thornton</td>
+                                <td>03:00</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Car Engine makes Weird sound</td>
+                                <td>Thornton</td>
+                                <td>03:00</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+
+
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>How to open E campus site</td>
+                                <td>Mark</td>
+                                <td>04:00</td>
+                            </tr>
+
+
+
+                        </tbody>
+                    </table>
+
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1">Previous</a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+
+    <div class="mt-5 p-4 bg-dark text-white text-center">
+        <p>created for 2sem project parul university</p>
+    </div>
+
+
+
+
+
+
+
+</body>
+
+</html>
